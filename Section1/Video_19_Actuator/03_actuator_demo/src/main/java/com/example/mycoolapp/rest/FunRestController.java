d3807a866e7c6964892a166294e35e1e313a4c1d
Stoyan Chompov <stoqnchompov@gmail.com>
2024-09-21 18:47:25 +0300

    Commit #3 We made some changes in application.properties, the changes follow: -- Showing how to include all or particular endpoints; -- Giving some info, that will be displayed in /actuator/info -- We imported the dependency for actuator, that allowed us to tap into the internal and external endpoints of our app;

---
+++ Section1/Video_19_Actuator/03_actuator_demo/src/main/java/com/example/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,30 @@
+package com.example.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //С тази анотация посочваме, че тоза клас ще бъде RestController
+public class FunRestController {
+
+    // expose endpoint "/" that returns "Hello World"
+
+    @GetMapping("/") //HTTP GET Request
+    public String sayHello() {
+        return "Hello World";
+    }
+
+
+    //Exposing new ENDPOINT
+    @GetMapping("/workout") //HTTP GET Request
+    public String getDailyWorkout() {
+        return "Run A HArd 5k!";
+    }
+
+    //Exposing a new ENDPOINT
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
+    }
+
+}
